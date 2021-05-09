7dd0ee4e119d903a17204f912ea73d68d23686a7
Juan José Muñoz Arenas <juanjomz@iesnervion.es>
2021-05-09 21:10:20 +0200

    Add files via upload

---
+++ TEMA 10  PROGRAMACION SQL, FUNCIONES Y PROCEDIMIENTOS/posiblesConsultasExamen.sql
@@ -0,0 +1,38 @@
+
+SELECT*FROM HGeneros
+GO
+CREATE FUNCTION FPeliculaMasConsumidaYGenero(@Nombre varchar(20)) 
+RETURNS TABLE AS RETURN
+
+	SELECT P.nombre,PG.IDPelicula,PG.IDGenero,COUNT(*) FROM HPerfiles AS P
+	INNER JOIN HVisionados AS V ON P.ID=V.IDPerfil INNER JOIN HContenidos AS C ON V.IDContenido=C.ID
+	INNER JOIN HPeliculas AS PC ON C.ID=PC.ID INNER JOIN HPeliculasGeneros AS PG ON PC.ID=PG.IDPelicula
+	GROUP BY P.nombre,PG.IDGenero,PG.IDPelicula--PELICULA CON SU GENERO 
+
+
+	SELECT P.nombre,PG.IDGenero,COUNT(*) FROM HPerfiles AS P
+	INNER JOIN HVisionados AS V ON P.ID=V.IDPerfil INNER JOIN HContenidos AS C ON V.IDContenido=C.ID
+	INNER JOIN HPeliculas AS PC ON C.ID=PC.ID INNER JOIN HPeliculasGeneros AS PG ON PC.ID=PG.IDPelicula
+	GROUP BY P.nombre,PG.IDGenero--LO MISMO PERO SIN LA PELICULA SERÍA EL GENERO MÁS CONSUMIDO
+
+	--A AMBOS  QUEDARÍA HACERLE EL MAX Y TAL BASICAMENTE UNOS SUPERVENTAS
+
+GO
+SELECT*FROM HPerfiles
+
+SELECT*FROM HSeries
+SELECT*FROM HTiposSuscripcion
+SELECT*FROM HPeliculas
+
+SELECT NVV.ID,MAX(NV.NVisionados) AS NVisionados,POS.IDSerie,POS.titulo FROM 
+(SELECT C.ID,COUNT(*) AS NVisionados FROM HVisionados AS V INNER JOIN HContenidos AS C
+ON V.IDContenido=C.ID 
+GROUP BY C.ID) AS NV INNER JOIN 
+(SELECT C.ID,COUNT(*) AS NVisionados FROM HVisionados AS V INNER JOIN HContenidos AS C
+ON V.IDContenido=C.ID 
+GROUP BY C.ID)AS NVV ON NV.NVisionados=NVV.NVisionados
+INNER JOIN(
+SELECT C.ID,HC.IDSerie,P.titulo FROM HContenidos AS C INNER JOIN HCapitulos AS HC ON C.ID=HC.ID
+INNER JOIN HPeliculas AS P ON C.ID=P.ID
+) AS POS ON NVV.ID=POS.ID
+GROUP BY NVV.ID,POS.IDSerie,POS.titulo
